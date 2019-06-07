ddd9a2d0097e6af2f7aa1b0912b6f08528e686a3
= <=>
2019-06-07 19:58:24 +0700

    backend

---
+++ public/precache-manifest.5e52698a0b317ba01b24af0c4c1ca7d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6680982984368127cfda5e0f1513d342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a761e01e7d982af509a2",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "099e623c6c491d3d7eb5",
+    "url": "/static/css/main.8d88d151.chunk.css"
+  },
+  {
+    "revision": "a761e01e7d982af509a2",
+    "url": "/static/js/2.42d165f0.chunk.js"
+  },
+  {
+    "revision": "099e623c6c491d3d7eb5",
+    "url": "/static/js/main.81dac521.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
